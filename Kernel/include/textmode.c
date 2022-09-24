d938a5f754f0ea1710367018dca6117ae68fbcba
Medhi Michel <100583551+toude-Archs@users.noreply.github.com>
2022-09-24 18:37:50 +0200

    Add files via upload

---
+++ Kernel/include/textmode.c
@@ -0,0 +1,74 @@
+#include <stddef.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "textmode.h"
+
+volatile uint16_t* VgaBuffer = (uint16_t*)0xB8000;
+//VGACols and VgaRows is for GUI And 0x0F is for Color
+const int VgaCols = 80;
+const int VgaRows = 25;
+uint8_t TerminalColor = 0x0F;
+//Terminal Color Is Color For Text
+
+
+int TerminalCol = 0;
+int TerminalRow = 0;
+//Is for Clear Qemu information 
+void TerminalClear() {
+    for (int col = 0; col < VgaCols; col ++)
+    {
+        for (int row = 0; row < VgaRows; row ++)
+        {
+            const size_t index = (VgaCols * row) + col;
+            VgaBuffer[index] = ((uint16_t)TerminalColor << 8) | ' ';
+        }
+    }
+    TerminalCol = 0;
+    TerminalRow = 0;
+}
+
+/*
+TerminalSetColor for turn TerminalColor
+*/
+
+
+void TerminalSetColor(uint8_t Color){
+    TerminalColor = Color;
+}
+
+void TerminalPutChar(char c){
+    switch (c){
+    case '\n':
+        {
+            TerminalCol = 0;
+            TerminalRow ++;
+            break;
+        }
+ 
+    default:
+        {
+            const size_t index = (VgaCols * TerminalRow) + TerminalCol;
+            VgaBuffer[index] = ((uint16_t)TerminalColor << 8) | c;
+            TerminalCol ++;
+            break;
+        }
+    }
+
+    if (TerminalCol >= VgaCols)
+    {
+        TerminalCol = 0;
+        TerminalRow ++;
+    }
+
+    if (TerminalRow >= VgaRows)
+    {
+        TerminalCol = 0;
+        TerminalRow = 0;
+    }
+}
+
+void TerminalWrite(const char* String){
+    for (size_t i = 0; String[i] != '\0'; i ++)
+        TerminalPutChar(String[i]);
+}
